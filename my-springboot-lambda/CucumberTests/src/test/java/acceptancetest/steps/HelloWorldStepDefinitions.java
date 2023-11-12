036dc161d6402a168a04c1a36c87184cd9a6182e
josephrobertlopez <josephrobertlopez@gmail.com>
2023-11-12 16:56:06 -0600

    Update HelloWorldStepDefinitions.java

--- my-springboot-lambda/CucumberTests/src/test/java/acceptancetest/steps/HelloWorldStepDefinitions.java
+++ my-springboot-lambda/CucumberTests/src/test/java/acceptancetest/steps/HelloWorldStepDefinitions.java
@@ -8,8 +8,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class HelloWorldStepDefinitions {
 
@@ -17,16 +16,21 @@ public class HelloWorldStepDefinitions {
     private String endpoint;
     private Response response;
 
-//    @Given("the API is running at {string}")
-//    public void theAPIIsRunningAt(String apiUrl) {
-//        this.apiUrl = apiUrl;
-//    }
+    @Given("the API is running at {string}")
+    public void theAPIIsRunningAt(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
 
     @When("a GET request is made to {string}")
     public void aGETRequestIsMadeTo(String endpoint) {
         this.endpoint = endpoint;
         RequestSpecification request = given();
         response = request.when().get(apiUrl + endpoint);
+    }
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int expectedStatusCode) {
+        assertNotNull("Response is null", response);
+        assertEquals("Unexpected status code", expectedStatusCode, response.getStatusCode());
     }
 
     @Then("the response body should contain a JSON object with the following properties:")
