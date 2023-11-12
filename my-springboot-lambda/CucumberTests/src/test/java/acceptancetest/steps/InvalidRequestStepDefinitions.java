61ba0b169aaa0bf86e753badb85ed84ef2f4c38b
josephrobertlopez <josephrobertlopez@gmail.com>
2023-11-12 17:14:45 -0600

    Update InvalidRequestStepDefinitions.java

--- my-springboot-lambda/CucumberTests/src/test/java/acceptancetest/steps/InvalidRequestStepDefinitions.java
+++ my-springboot-lambda/CucumberTests/src/test/java/acceptancetest/steps/InvalidRequestStepDefinitions.java
@@ -1,5 +1,7 @@
 package acceptancetest.steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -12,10 +14,11 @@ import static org.junit.Assert.assertNotNull;
 
 public class InvalidRequestStepDefinitions {
 
-    private Response response;
+    private Response mockedResponse;
+
     private ResponseBody mockedResponseBody;
 
-    @Given("the API is running at {string}")
+    @Given("the API is hosted at {string}")
     public void theApiIsRunningAt(String baseUrl) {
         RestAssured.baseURI = baseUrl;
     }
@@ -25,7 +28,7 @@ public class InvalidRequestStepDefinitions {
         try {
             // Mock the behavior of the REST client using Mockito
             RequestSpecification requestSpecification = Mockito.mock(RequestSpecification.class);
-            Response mockedResponse = Mockito.mock(Response.class);
+            mockedResponse = Mockito.mock(Response.class);
             mockedResponseBody = Mockito.mock(ResponseBody.class);
 
             Mockito.when(requestSpecification.post(endpoint)).thenReturn(mockedResponse);
@@ -36,33 +39,33 @@ public class InvalidRequestStepDefinitions {
             Mockito.when(mockedResponse.getStatusCode()).thenReturn(403);
 
             // Perform the mock request
-            response = requestSpecification.post(endpoint);
+            mockedResponse = requestSpecification.post(endpoint);
         } catch (Exception e) {
             // Log the exception or handle it as needed
             e.printStackTrace();
         }
     }
 
-    @Then("the response status code should be {int}")
+    @Then("the response statuscode should be {int}")
     public void theResponseStatusCodeShouldBe(int expectedStatusCode) {
-        assertNotNull("Response is null", response);
-        assertEquals("Unexpected status code", expectedStatusCode, response.getStatusCode());
+        assertNotNull("dResponse is null", mockedResponse);
+        assertEquals("Unexpected status code", expectedStatusCode, mockedResponse.getStatusCode());
     }
 
     @Then("the response body should contain a JSON object with the following property:")
-    public void theResponseBodyShouldContainAJSONObjectWithTheFollowingProperty(io.cucumber.datatable.DataTable dataTable) {
-        assertNotNull("Response body is null", response.getBody());
+    public void theResponseBodyShouldContainAJSONObjectWithTheFollowingProperty(DataTable dataTable) {
+        assertNotNull("Response body is null", mockedResponse.getBody());
 
-        // Convert DataTable to Map for easy comparison
-        java.util.Map<Object, Object> expectedJsonMap = dataTable.asMap(String.class, String.class);
+        try {
+            // Convert DataTable to JSON string
+            ObjectMapper objectMapper = new ObjectMapper();
+            String expectedJsonString = objectMapper.writeValueAsString(dataTable.asMap(String.class, String.class));
 
-        // Convert the expected JSON map to a string
-        String expectedJsonString = mockedResponseBody.toString();
-
-        // Convert the actual JSON string from the response body
-        String actualJsonString = response.getBody().toString();
-
-        // Compare the expected and actual JSON strings
-        assertEquals("Response body does not match expected JSON", expectedJsonString, actualJsonString);
+            // Compare the expected and actual JSON strings
+            assertEquals("Response body does not match expected JSON", expectedJsonString, mockedResponseBody.toString());
+        } catch (Exception e) {
+            // Handle the exception as needed
+            e.printStackTrace();
+        }
     }
 }
