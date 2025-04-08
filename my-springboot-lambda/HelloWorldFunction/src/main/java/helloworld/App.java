038479bb33b045edd1ccfa3e82d78ca4837cfdc3
Lopez <joseph.a.lopez@accenture.com>
2025-04-08 13:07:20 -0500

    java 17 changes

--- my-springboot-lambda/HelloWorldFunction/src/main/java/helloworld/App.java
+++ my-springboot-lambda/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -12,24 +12,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import software.amazon.lambda.powertools.logging.Logging;
-import software.amazon.lambda.powertools.metrics.Metrics;
-import software.amazon.lambda.powertools.tracing.CaptureMode;
-import software.amazon.lambda.powertools.tracing.Tracing;
-
-import static software.amazon.lambda.powertools.tracing.CaptureMode.*;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    Logger log = LogManager.getLogger(App.class);
 
-    @Logging(logEvent = true)
-    @Tracing(captureMode = DISABLED)
-    @Metrics(captureColdStart = true)
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
@@ -50,11 +38,10 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                     .withStatusCode(500);
         }
     }
-    @Tracing(namespace = "getPageContents")
-    private String getPageContents(String address) throws IOException {
-        log.info("Retrieving {}", address);
+
+    private String getPageContents(String address) throws IOException{
         URL url = new URL(address);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
